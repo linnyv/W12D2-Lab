35cc120d4f069c3cb6aae452cdaa380e0a19e5e1
linnyv <133271009+linnyv@users.noreply.github.com>
2023-08-14 10:50:24 -0400

    Updated responses/repuestas for Magic 8 Ball.

--- server.js
+++ server.js
@@ -39,24 +39,27 @@ app.get('/magic/:question', (req, res) => {
     const preguntas = req.params.question;
 
 
-    const respuestas = [
-        `Maybe`,
-        `No`,
-        `Si`,
-        `Yes`,
-        `Tal Vez`,
-        `Not sure`,
-        `Definietly`,
-        `Definietly not`,
-        `Likely`,
-        `Not Likely`,
-        `Possible`,
-        `Not Possible`,
-        `Certainly`,
-        `Ask later`,
-        `Ask tomorrow`,
-        `Ask in the future`,
-    ];
+    const respuestas = ["It is certain", 
+    "It is decidedly so", 
+    "Without a doubt", 
+    "Yes definitely",
+    "You may rely on it", 
+    "As I see it yes", 
+    "Most likely", 
+    "Outlook good",
+    "Yes", 
+    "Signs point to yes", 
+    "Reply hazy try again", 
+    "Ask again later",
+    "Better not tell you now", 
+    "Cannot predict now", 
+    "Concentrate and ask again",
+    "Don't count on it", 
+    "My reply is no", 
+    "My sources say no",
+    "Outlook not so good", 
+    "Very doubtful"];
+
     const randomizeRespuestas = respuestas[Math.floor(Math.random() * respuestas.length)];
 
 
